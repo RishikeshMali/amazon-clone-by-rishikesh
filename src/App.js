e61ce11a5bfb4747cbf697e6fca53b4a735f38fd
RishikeshMali <rishikeshmali924@gmail.com>
2022-03-13 17:37:53 +0530

    Added Routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+          <h1>Building Amazon Clone</h1>
+          <Routes>
+
+            <Route exact path="/checkout"
+            element = {<h1>Checkout</h1>}>
+            </Route>
+
+            <Route exact path="/login"
+            element = {<h1>Login</h1>}>
+            </Route>
+
+            <Route exact path="/"
+            element = {<h1>Home</h1>}>
+            </Route>
+
+          </Routes>
+      </div>
+    
+    </Router>
   );
 }
 
